1c48788ba91bea207842cd490c5274629414598b
jhaasdijk <j.haasdijk@student.ru.nl>
2021-02-12 22:53:52 +0000

    Include 'common' directory
    The 'common' directory currently contains make.mk which consists of
    reusable Makefile components. This directory can serve as a basis, not
    only for future Makefiles, but also for the hardware abstraction layer
    (HAL). A place to put reusable components.

---
+++ samples/common/make.mk
@@ -0,0 +1,40 @@
+# Shared Makefile components which can be reused throughout the repository
+# Tested and used with GNU Make 4.2.1
+
+# Ensure strict POSIX make rules.
+.POSIX :
+
+# Empty the the list of default suffixes
+.SUFFIXES :
+# Include C files, sources
+.SUFFIXES : .c .h
+# Include C products, object files, make dependencies, assembler
+.SUFFIXES : .o .d .s
+
+# Choice of compiler, linker, utilities for object files
+CC = gcc
+LD = gcc
+OBJCOPY = objcopy
+OBJDUMP = objdump
+
+# Turn on all optimizations specified by -O2 and more
+OPT = -O3 -march=native -mcpu=cortex-a72
+
+# Turn on extended warnings
+WAR = -Wall -Wextra -Wmissing-prototypes -Wredundant-decls\
+	-Wstrict-prototypes -Wundef -Wshadow -Wconversion -Wfloat-equal\
+	-Wpointer-arith -Wcast-align -Wswitch-default -Wswitch-enum\
+	-Wunreachable-code
+
+# Defining a C standard to which we conform
+STD = -std=gnu17
+
+# Generate a GNU Make dependency output file
+DEP = -MD
+
+CFLAGS = ${OPT} ${WAR} ${STD} ${DEP}
+
+LDFLAGS = -Wl,--start-group -lc -lgcc -Wl,--end-group\
+	-Wl,--gc-sections,--print-gc-sections
+
+LDLIBS =
