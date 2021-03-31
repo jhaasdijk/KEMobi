e825f7cd101b29627da8748f55bf1c56dfb83f2d
jhaasdijk <j.haasdijk@student.ru.nl>
2021-03-31 14:34:53 +0200

    Add *working* size-11 Goods NTT
    This script performs the NTT based polynomial multiplication of two
    polynomials of size 11, using Good's trick. The various FIXME tags have
    been changed to FIXED wherever potential issues were resolved or
    additional notes were required.

--- samples/polymult_ntt/ntt11.py
+++ samples/polymult_ntt/ntt11.py
@@ -1,166 +1,127 @@
 #!/usr/bin/env python3
 
-import math
-
-# FIXME :: Does not yet work as expected.
+from common import Goods
+from common import NTT
+from common import pad
+from common import reduce_q
 
 """
 This script can be used to perform NTT based polynomial multiplication of two
 polynomials of size 11. Throughout this file we are multiplying A and B to 
 produce C. Variable names are generally named after A,B,C using _<T> notation to
-indicate their current format. The required steps are:
-
-1. Pad polynomials A, B to size 24 (with zeros at the start)
-2. Perform Good's permutation of A and B to obtain 3 size-8 polynomials each
-3. Perform 3 size-8 forward NTTs
-4. Calculate point-wise multiplication of the coefficients
-5. Inverse the size-8 NTTs of the result
-6. Undo Good's permutation
-7. Reduce mod (x^11 - x - 1)
+indicate their current format. The main steps of the algorithm have been 
+highlighted using " -- " comments
 """
 
-# Define the original (P, Q) and NTT 'suitable' (P0, P1, P0P1) parameters
-P, Q, P0, P1, P0P1 = 11, 17, 3, 8, 24
+# Define the original (p, q) and NTT 'suitable' (p0, p1, p0p1) parameters
+p, q = 11, 17
+p0, p1, p0p1 = 3, 8, 24
 
-# We have polynomials A, B of size 11 (size P)
-A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-B = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-
-# FIXME :: When multiplying in the smaller NTTs we need a new Q' != 17
+# FIXED :: When multiplying in the smaller NTTs we need a new Q' != 17
 #       :: This Q' needs to be at least (2 * P - 1) * (Q / 2) * 2
-#       :: This requires a different roots and roots_inv
+#       :: How do we determine this Q' ?
+#       :: This requires different roots and roots_inv
+#
+# When multiplying in the smaller NTTs we DO NOT need a new Q' per se. We
+# only need a new Q' when our original Q does not provide us with the roots
+# of unity. Since Q = 17 DOES provide us with the required roots, there is no
+# need to construct a new Q'.
 
-# roots when multiplying in Z_17 [x] / (x^8 - 1)
+# FIXED :: Are we reducing mod (x^8 - 1) or mod (x^8 + 1) in the smaller NTTs?
+#       :: This changes what roots of unity we are multiplying with
+#
+# We are reducing mod (x^8 - 1)
+
+# Roots when multiplying in Z_17 [x] / (x^8 - 1)
 roots = [16, 4, 16, 2, 8, 4, 16]  # nth roots of unity
 roots_inv = [16, 13, 16, 9, 15, 13, 16]  # inverse nth roots of unity
 
-# 2^{-LAY} mod Q = 2^{-3} mod 17 = 8^{-1} mod 17 = 15
-LAY = int(math.log2(P1))  # needs explicit conversion to integer
-LAY_INV = pow(2, -LAY, Q)  # this only works in Python3.8+
+# Setup Good's and NTT parameters
+goods = Goods(p0, p1, p0p1)
+ntt = NTT(q, p1, roots, roots_inv)
 
+" -- Define polynomials A, B of size p "
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+B = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-# FIXME :: When reducing in the smaller NTTs we need to reduce mod Q'
-#       :: When reducing outside of the NTTs we need to reduce mod Q
-def reduce_q(cvec):
-    """ Reduction of all integer coefficients mod Q """
-    return list(map(lambda x: x % Q, cvec))
+" -- Zero pad polynomials A, B to size p0p1 "
+A_PAD = pad(A, p0p1)
+B_PAD = pad(B, p0p1)
 
+" -- Perform Good's permutation to obtain p0 size-p1 polynomials each "
+A_PAD_G = goods.forward(A_PAD)
+B_PAD_G = goods.forward(B_PAD)
 
-def forward(cvec, ridx=0):
-    """
-    Calculate forward NTT of a polynomial represented by its coefficient vector
-    :param cvec: Polynomial represented by its coefficient vector
-    :param ridx: Index used to point to the root of unity, default 0
-    :return: The forward NTT transform (of the left and right halves)
-    """
-    if len(cvec) == 1:
-        return cvec
-    else:
-        half = math.floor(len(cvec) / 2)
-        cvec_l, cvec_r = [0] * half, [0] * half
+" -- Perform p0 size-p1 forward NTTs "
+A_PAD_G_F = [
+    ntt.forward(A_PAD_G[0]),
+    ntt.forward(A_PAD_G[1]),
+    ntt.forward(A_PAD_G[2])
+]
+B_PAD_G_F = [
+    ntt.forward(B_PAD_G[0]),
+    ntt.forward(B_PAD_G[1]),
+    ntt.forward(B_PAD_G[2])
+]
 
-        for idx in range(half):
-            cvec_l[idx] = cvec[idx] + roots[ridx] * cvec[idx + half]
-            cvec_r[idx] = cvec[idx] - roots[ridx] * cvec[idx + half]
+" -- Calculate 'point-wise' multiplication of the coefficients "
+"""
+Note that the 'smaller' polynomial multiplications are not 'normal', as we are
+not actually computing the result 'point-wise'. Instead we multiply two degree 
+2 polynomials and reduce the result mod (x^3 - 1). E.g.:
 
-        cvec_l = reduce_q(cvec_l)
-        cvec_r = reduce_q(cvec_r)
+( [A[0][0], A[1][0], A[2][0]] * [B[0][0], B[1][0], B[2][0]] ) % (X^3 - 1)
+= C[0][0], C[1][0], C[2][0]
+"""
 
-        return forward(cvec_l, ridx * 2 + 1) + forward(cvec_r, ridx * 2 + 2)
+# define variable for storing the result of the computation
+C_PAD_G_F = [[0 for _ in range(p1)] for _ in range(p0)]
 
+for i in range(p1):
 
-def invert(cvec, ridx=0):
-    """
-    Calculate inverse NTT of a polynomial represented by its coefficient vector
-    :param cvec: Polynomial represented by its coefficient vector
-    :param ridx: Index used to point to the inverse root of unity, default 0
-    :return: The inverse NTT transform
-    """
-    if len(cvec) == 1:
-        return cvec
-    else:
-        half = math.floor(len(cvec) / 2)
-        cvec_l = invert(cvec[:half], ridx * 2 + 1)
-        cvec_r = invert(cvec[half:], ridx * 2 + 2)
+    # define an accumulator to store temporary values
+    accum = [0 for _ in range(2 * p0)]
 
-        rvec = [0] * P1  # storing the result, we do not overwrite cvec
-        for idx in range(half):
-            rvec[idx] = cvec_l[idx] + cvec_r[idx]
-            rvec[idx + half] = (cvec_l[idx] - cvec_r[idx]) * roots_inv[ridx]
+    # obtain two degree 2 polynomials from A, B
+    poly_a = [A_PAD_G_F[0][i], A_PAD_G_F[1][i], A_PAD_G_F[2][i]]
+    poly_b = [B_PAD_G_F[0][i], B_PAD_G_F[1][i], B_PAD_G_F[2][i]]
 
-        return rvec
+    # multiply the two polynomials naively
+    for n in range(p0):
+        for m in range(p0):
+            accum[n + m] += poly_a[n] * poly_b[m]
 
+    # reduce mod (x^3 - 1)
+    for ix in range(2 * p0 - 1, p0 - 1, -1):
+        if accum[ix] > 0:  # x^p is nonzero
+            accum[ix - p0] += accum[ix]  # add x^p into x^0
+            accum[ix] = 0  # zero x^p
 
-# Zero pad polynomials A, B to size 24
-A_PAD = A + [0] * (P0P1 - len(A))
-B_PAD = B + [0] * (P0P1 - len(B))
+    # store the result
+    accum = reduce_q(accum, q)
+    C_PAD_G_F[0][i] = accum[0]
+    C_PAD_G_F[1][i] = accum[1]
+    C_PAD_G_F[2][i] = accum[2]
 
-# Perform Good's permutation to obtain 3 size-8 polynomials each
-A_PAD_G = [[0] * P1, [0] * P1, [0] * P1]
-B_PAD_G = [[0] * P1, [0] * P1, [0] * P1]
+" -- Inverse the size-8 NTTs "
+C_PAD_G = [
+    ntt.inverse(C_PAD_G_F[0]),
+    ntt.inverse(C_PAD_G_F[1]),
+    ntt.inverse(C_PAD_G_F[2])
+]
 
-for i in range(P0P1):
-    lane = i % P0  # determines in which NTT 'lane' the coefficient ends up
-    coef = i % P1  # determines which 'lane' coefficient is used
-    A_PAD_G[lane][coef] = A_PAD[i]
-    B_PAD_G[lane][coef] = B_PAD[i]
+" -- Undo Good's permutation "
+C_PAD = goods.inverse(C_PAD_G)
 
-# Perform 3 size-8 forward NTTs each
-A_PAD_G_F = [forward(A_PAD_G[0]), forward(A_PAD_G[1]), forward(A_PAD_G[2])]
-B_PAD_G_F = [forward(B_PAD_G[0]), forward(B_PAD_G[1]), forward(B_PAD_G[2])]
-
-# FIXME :: What kind of multiplication are we doing exactly?
-#       :: Why are the 'smaller' polynomial multiplications not 'normal' ?
-
-# Perform the point-wise multiplication of the coefficients
-C_PAD_G_F = [[0] * P1, [0] * P1, [0] * P1]
-
-# option 1 : c[0][0] = a[0][0]*b[0][0] + a[0][0]*b[1][0] + a[0][0]*b[2][0]
-for i in range(P0):
-    for j in range(P1):
-        for k in range(P0):
-            C_PAD_G_F[i][j] += A_PAD_G_F[i][j] * B_PAD_G_F[k][j]
-
-# option 2 : c[0][0] = a[0][0]*b[0][0]
-# for i in range(P0):
-#     # CPAD_G_F[i] = [(a * b) % Q for a, b in zip(APAD_G_F[i], BPAD_G_F[i])]
-#     CPAD_G_F[i] = [(a * b) for a, b in zip(APAD_G_F[i], BPAD_G_F[i])]
-
-# Inverse the size-8 NTTs
-C_PAD_G = [invert(C_PAD_G_F[0]), invert(C_PAD_G_F[1]), invert(C_PAD_G_F[2])]
-for i in range(P0):
-    # CPAD_G[i] = list(map(lambda x: (x * INVLAY) % Q, CPAD_G[i]))
-    C_PAD_G[i] = list(map(lambda x: (x * LAY_INV), C_PAD_G[i]))
-
-# Undo Good's permutation
-C_PAD = [0] * P0P1
-for i in range(P0P1):
-    lane = i % P0  # determines in which NTT 'lane' the coefficient ends up
-    coef = i % P1  # determines which 'lane' coefficient is used
-    C_PAD[i] = C_PAD_G[lane][coef]
-
-# Reduce all integer coefficients mod Q
-C_PAD = reduce_q(C_PAD)
-
-# Reduce mod (x^11 - x - 1)
-for i in range(P0P1 - 1, P - 1, -1):
+" -- Reduce mod (x^11 - x - 1) "
+for i in range(p0p1 - 1, p - 1, -1):
     if C_PAD[i] > 0:  # x^p is nonzero
-        C_PAD[i - (P - 1)] += C_PAD[i]  # add x^p into x^1
-        C_PAD[i - P] += C_PAD[i]  # add x^p into x^0
+        C_PAD[i - p + 1] += C_PAD[i]  # add x^p into x^1
+        C_PAD[i - p] += C_PAD[i]  # add x^p into x^0
         C_PAD[i] = 0  # zero x^p
 
-C = reduce_q(C_PAD)[:P]
+" -- Store the result "
+C = reduce_q(C_PAD, q)[:p]
 
-# Printing intermediate values
-print(f"A:         {A}")
-print(f"B:         {B}")
-print(f"A_PAD:     {A_PAD}")
-print(f"B_PAD:     {B_PAD}")
-print(f"A_PAD_G:   {A_PAD_G}")
-print(f"B_PAD_G:   {B_PAD_G}")
-print(f"A_PAD_G_F: {A_PAD_G_F}")
-print(f"B_PAD_G_F: {B_PAD_G_F}")
-print(f"C_PAD_G_F: {C_PAD_G_F}")
-print(f"C_PAD_G:   {C_PAD_G}")
-print(f"C_PAD:     {C_PAD}")
-print(f"C:         {C}")
+# 7x^10 + 7x^9 + 10x^8 + 15x^7 + 4x^6 + 10x^5 + 15x^4 + x^3 + x^2 + 14x + 9
+print(f"The result is {C == [9, 14, 1, 1, 15, 10, 4, 15, 10, 7, 7]}")
