bac2656bc12e6b4a67a92dd6f44d2952f1d6b268
jhaasdijk <j.haasdijk@student.ru.nl>
2021-03-31 15:09:17 +0200

    Update size-4 and size-8 NTT based polynomial multiplication
    Both scripts now use the updated layout made possible with the 'common'
    module.

--- samples/polymult_ntt/ntt04.py
+++ samples/polymult_ntt/ntt04.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import math
+from common import NTT
 
 """
 This script can be used for multiplying two polynomials using NTT based
@@ -9,76 +9,29 @@ means that we have two polynomials with integer coefficients from {0, 1, ...,
 16}. The polynomials are reduced mod (x^4 + 1), i.e. x^4 = -1.
 """
 
-PRIMEP = 4
-PRIMEQ = 17
+PRIMEP, PRIMEQ = 4, 17
 
-# 2^{-LAY} mod PRIMEQ = 2^{-2} mod 17 = 4^{-1} mod 17 = 13
-LAY = int(math.log2(PRIMEP))  # needs explicit conversion to integer
-INVLAY = pow(2, -LAY, PRIMEQ)  # this only works in Python3.8+
+# Define polynomials A, B of size 4
+A = [2, 0, 0, 7]  # 2 + 7x^3
+B = [6, 0, 2, 0]  # 6 + 2x^2
 
-poly_a = [2, 0, 0, 7]  # 2 + 7x^3
-poly_b = [6, 0, 2, 0]  # 6 + 2x^2
-
+# Roots when multiplying in Z_17 [x] / (x^4 + 1)
 roots = [4, 2, 8]  # nth roots of unity
 roots_inv = [13, 9, 15]  # inverse nth roots of unity
 
+# Setup our NTT transform (parameters)
+ntt = NTT(PRIMEQ, PRIMEP, roots, roots_inv)
 
-def reduce(cvec):
-    """ Reduction of all integer coefficients mod PRIMEQ """
-    return list(map(lambda x: x % PRIMEQ, cvec))
+# Calculate the forward NTT of polynomials A, B
+A_F = ntt.forward(A)
+B_F = ntt.forward(B)
 
-
-def forward(cvec, ridx=0):
-    """ Calculate the forward NTT of polynomial cvec """
-    if len(cvec) == 1:
-        return cvec
-    else:
-        half = math.floor(len(cvec) / 2)
-        cvec_l, cvec_r = [0] * half, [0] * half
-
-        for idx in range(half):
-            cvec_l[idx] = cvec[idx] + roots[ridx] * cvec[idx + half]
-            cvec_r[idx] = cvec[idx] - roots[ridx] * cvec[idx + half]
-
-        cvec_l = reduce(cvec_l)
-        cvec_r = reduce(cvec_r)
-
-        return forward(cvec_l, ridx * 2 + 1) + forward(cvec_r, ridx * 2 + 2)
-
-
-# Calculate the forward NTT of polynomials a, b
-fa = forward(poly_a)
-fb = forward(poly_b)
-
-# Calculate the point-wise multiplication of fa, fb
+# Calculate the point-wise multiplication of A_F, B_F
 # The integer coefficients are reduced Z_q
-fc = [(a * b) % PRIMEQ for a, b in zip(fa, fb)]
+C_F = [(a * b) % PRIMEQ for a, b in zip(A_F, B_F)]
 
-# Calculate the inverse NTT of polynomial fc
-# Unrolled loop - Layer 1
-im = [0] * PRIMEP
-im[0] = fc[0] + fc[1]
-im[1] = (fc[0] - fc[1]) * roots_inv[1]
-im[2] = fc[2] + fc[3]
-im[3] = (fc[2] - fc[3]) * roots_inv[2]
+# Calculate the inverse NTT of polynomial C_F
+C = ntt.inverse(C_F)
 
-# Unrolled loop - Layer 2
-poly_p = [0] * PRIMEP
-poly_p[0] = im[0] + im[2]
-poly_p[1] = im[1] + im[3]
-poly_p[2] = (im[0] - im[2]) * roots_inv[0]
-poly_p[3] = (im[1] - im[3]) * roots_inv[0]
-
-# Multiply every coefficient with 2^{-l}
-poly_p = list(map(lambda x: (x * INVLAY) % PRIMEQ, poly_p))
-
-# Print intermediate values
-print(f"Polynomial A : {poly_a}")
-print(f"Polynomial B : {poly_b}")
-print()
-print(f"Forward NTT of A : {fa}")
-print(f"Forward NTT of B : {fb}")
-print()
-print(f"Multiplied : {fc}")
-print()
-print(f"Result : {poly_p}")
+print(f"Calculating : Zx({A}) * Zx({B}) % (x^4 + 1) % 17")
+print(f"Produces    : {C}")
--- samples/polymult_ntt/ntt08.py
+++ samples/polymult_ntt/ntt08.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import math
+from common import NTT
 
 """
 This script can be used for multiplying two polynomials using NTT based
@@ -9,91 +9,29 @@ means that we have two polynomials with integer coefficients from {0, 1, ...,
 16}. The polynomials are reduced mod (x^8 + 1), i.e. x^8 = -1.
 """
 
-PRIMEP = 8
-PRIMEQ = 17
+PRIMEP, PRIMEQ = 8, 17
 
-# 2^{-LAY} mod PRIMEQ = 2^{-3} mod 17 = 8^{-1} mod 17 = 15
-LAY = int(math.log2(PRIMEP))  # needs explicit conversion to integer
-INVLAY = pow(2, -LAY, PRIMEQ)  # this only works in Python3.8+
+# Define polynomials A, B of size 8
+A = [2, 0, 0, 7, 2, 0, 0, 7]  # 2 + 7x^3 + 2x^4 + 7x^7
+B = [6, 0, 2, 0, 6, 0, 2, 0]  # 6 + 2x^2 + 6x^4 + 2x^6
 
-poly_a = [2, 0, 0, 7, 2, 0, 0, 7]  # 2 + 7x^3 + 2x^4 + 7x^7
-poly_b = [6, 0, 2, 0, 6, 0, 2, 0]  # 6 + 2x^2 + 6x^4 + 2x^6
-
+# Roots when multiplying in Z_17 [x] / (x^8 + 1)
 roots = [4, 2, 8, 6, 10, 5, 3]  # nth roots of unity
 roots_inv = [13, 9, 15, 3, 12, 7, 6]  # inverse nth roots of unity
 
+# Setup our NTT transform (parameters)
+ntt = NTT(PRIMEQ, PRIMEP, roots, roots_inv)
 
-def reduce(cvec):
-    """ Reduction of all integer coefficients mod PRIMEQ """
-    return list(map(lambda x: x % PRIMEQ, cvec))
+# Calculate the forward NTT of polynomials A, B
+A_F = ntt.forward(A)
+B_F = ntt.forward(B)
 
-
-def forward(cvec, ridx=0):
-    """
-    Calculate forward NTT of a polynomial represented by its coefficient vector
-    :param cvec: Polynomial represented by its coefficient vector
-    :param ridx: Index used to point to the root of unity, default 0
-    :return: The forward NTT transform (of the left and right halves)
-    """
-    if len(cvec) == 1:
-        return cvec
-    else:
-        half = math.floor(len(cvec) / 2)
-        cvec_l, cvec_r = [0] * half, [0] * half
-
-        for idx in range(half):
-            cvec_l[idx] = cvec[idx] + roots[ridx] * cvec[idx + half]
-            cvec_r[idx] = cvec[idx] - roots[ridx] * cvec[idx + half]
-
-        cvec_l = reduce(cvec_l)
-        cvec_r = reduce(cvec_r)
-
-        return forward(cvec_l, ridx * 2 + 1) + forward(cvec_r, ridx * 2 + 2)
-
-
-def invert(cvec, ridx=0):
-    """
-    Calculate inverse NTT of a polynomial represented by its coefficient vector
-    :param cvec: Polynomial represented by its coefficient vector
-    :param ridx: Index used to point to the inverse root of unity, default 0
-    :return: The inverse NTT transform
-    """
-    if len(cvec) == 1:
-        return cvec
-    else:
-        half = math.floor(len(cvec) / 2)
-        cvec_l = invert(cvec[:half], ridx * 2 + 1)
-        cvec_r = invert(cvec[half:], ridx * 2 + 2)
-
-        rvec = [0] * PRIMEP  # storing the result, we do not overwrite cvec
-        for idx in range(half):
-            rvec[idx] = cvec_l[idx] + cvec_r[idx]
-            rvec[idx + half] = (cvec_l[idx] - cvec_r[idx]) * roots_inv[ridx]
-
-        return rvec
-
-
-# Calculate the forward NTT of polynomials fa, fb
-fa = forward(poly_a)
-fb = forward(poly_b)
-
-# Calculate the point-wise multiplication of fa, fb
+# Calculate the point-wise multiplication of A_F, B_F
 # The integer coefficients are reduced Z_q
-fc = [(a * b) % PRIMEQ for a, b in zip(fa, fb)]
+C_F = [(a * b) % PRIMEQ for a, b in zip(A_F, B_F)]
 
-# Calculate the inverse NTT of polynomial fc
-poly_p = invert(fc)
+# Calculate the inverse NTT of polynomial C_F
+C = ntt.inverse(C_F)
 
-# Multiply every coefficient with 2^{-l}
-poly_p = list(map(lambda x: (x * INVLAY) % PRIMEQ, poly_p))
-
-# Print (intermediate) values
-print(f"Polynomial A : {poly_a}")
-print(f"Polynomial B : {poly_b}")
-print()
-print(f"Forward NTT of A : {fa}")
-print(f"Forward NTT of B : {fb}")
-print()
-print(f"Multiplied : {fc}")
-print()
-print(f"Result : {poly_p}")
+print(f"Calculating : Zx({A}) * Zx({B}) % (x^8 + 1) % 17")
+print(f"Produces    : {C}")
