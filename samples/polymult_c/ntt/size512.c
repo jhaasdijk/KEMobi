feed3da4abc908ab6ceb146c3211fa37d7f9a77c
jhaasdijk <j.haasdijk@student.ru.nl>
2021-05-18 10:01:56 +0100

    Clean kem/ntrulpr761 implementation
    This commit cleans the main flow of the kem/ntrulpr761 implementation.
    Individual steps and values have been explained extensively.
    
    Note that has definitely not (yet) been optimized for performance.
    Readability has so far been the main focus!

--- samples/polymult_c/ntt/size512.c
+++ samples/polymult_c/ntt/size512.c
@@ -701,113 +701,132 @@ void reduce_terms_761(int32_t *coefficients)
 
 int main()
 {
-    /* -- Zero pad polynomials poly_one, poly_two to size GPR "*/
+    /**
+     * @brief Zero pad the input polynomials to size 1536.
+     */
 
-    int32_t p1_pad[GPR];
-    int32_t p2_pad[GPR];
+    int32_t A_vec[GPR], B_vec[GPR];
 
-    pad(p1_pad, poly_one);
-    pad(p2_pad, poly_two);
+    pad(A_vec, poly_one);
+    pad(B_vec, poly_two);
 
-    /* -- Perform Good's permutation to obtain GP0 size-GP1 polynomials each "*/
+    /**
+     * @brief Compute the forward Good's permutation.
+     * 
+     * This deconstructs the 'clunky' zero padded arrays of integer coefficients
+     * into 3 size-512 NTTs.
+     */
 
-    int32_t p1_pad_g[GP0][GP1];
-    int32_t p2_pad_g[GP0][GP1];
+    int32_t A_mat[GP0][GP1], B_mat[GP0][GP1];
 
-    goods_forward(p1_pad_g, p1_pad);
-    goods_forward(p2_pad_g, p2_pad);
+    goods_forward(A_mat, A_vec);
+    goods_forward(B_mat, B_vec);
 
-    /* -- Perform GP0 size-GP1 forward NTTs "*/
+    /**
+     * @brief Compute the iterative inplace forward NTTs.
+     * 
+     * This computes the forward NTT tranformation of our size-512 polynomials.
+     */
 
-    ntt_forward(p1_pad_g[0], NTT_Q);
-    ntt_forward(p1_pad_g[1], NTT_Q);
-    ntt_forward(p1_pad_g[2], NTT_Q);
+    for (size_t idx = 0; idx < GP0; idx++)
+    {
+        ntt_forward(A_mat[idx], NTT_Q);
+        ntt_forward(B_mat[idx], NTT_Q);
+    }
 
-    ntt_forward(p2_pad_g[0], NTT_Q);
-    ntt_forward(p2_pad_g[1], NTT_Q);
-    ntt_forward(p2_pad_g[2], NTT_Q);
+    /**
+     * @brief Compute the point-wise multiplication of the integer coefficients.
+     * 
+     * Be careful with these smaller polynomial multiplications. We are not
+     * actually computing the result 'point-wise'. Instead we multiply two
+     * degree 2 polynomials and reduce the result mod (x^3 - 1). E.g.:
+     * 
+     * (
+     *   { F[0][0], F[1][0], F[2][0] } *
+     *   { G[0][0], G[1][0], G[2][0] }
+     * ) % (X^3 - 1)
+     * 
+     * = C[0][0], C[1][0], C[2][0]
+     */
 
-    /* TODO
--- Calculate 'point-wise' multiplication of the coefficients "
-Note that the 'smaller' polynomial multiplications are not 'normal', as we are
-not actually computing the result 'point-wise'. Instead we multiply two degree 
-2 polynomials and reduce the result mod (x^3 - 1). E.g.:
-
-( [A[0][0], A[1][0], A[2][0]] * [B[0][0], B[1][0], B[2][0]] ) % (X^3 - 1)
-= C[0][0], C[1][0], C[2][0]
-*/
-
-    int32_t C[GP0][GP1];
+    int32_t C_mat[GP0][GP1];
 
     for (size_t idx = 0; idx < GP1; idx++)
     {
-        // Define an accumulator to store temporary values
+        /* Define an accumulator to store temporary values. It is important that
+         * we (re)initialize this with zeros at each iteration of the loop */
         int32_t accum[2 * GP0 - 1] = {0, 0, 0, 0, 0};
 
-        // Obtain two degree 2 polynomials from A, B
-        int32_t A[GP0] = {p1_pad_g[0][idx], p1_pad_g[1][idx], p1_pad_g[2][idx]};
-        int32_t B[GP0] = {p2_pad_g[0][idx], p2_pad_g[1][idx], p2_pad_g[2][idx]};
+        /* Obtain two degree 2 polynomials from A_mat, B_mat */
+        int32_t F[GP0] = {A_mat[0][idx], A_mat[1][idx], A_mat[2][idx]};
+        int32_t G[GP0] = {B_mat[0][idx], B_mat[1][idx], B_mat[2][idx]};
 
-        // Multiply the two polynomials naively
+        /* Multiply the two polynomials naively */
         for (size_t n = 0; n < GP0; n++)
         {
             for (size_t m = 0; m < GP0; m++)
             {
-                accum[n + m] += multiply_modulo(A[n], B[m], NTT_Q);
+                accum[n + m] += multiply_modulo(F[n], G[m], NTT_Q);
             }
         }
 
-        // Reduce mod (x^3 - 1)
-        for (size_t idx = 2 * GP0 - 2; idx > GP0 - 1; idx--)
+        /* Reduce the result mod (x^3 - 1) */
+        for (size_t idx = 2 * GP0 - 2; idx >= GP0; idx--)
         {
             if (accum[idx] > 0)
-            {                                   // x^p is nonzero
-                accum[idx - GP0] += accum[idx]; // add x^p into x^0
-                accum[idx] = 0;                 // zero x^p
+            {                                   /* x^p is nonzero */
+                accum[idx - GP0] += accum[idx]; /* add x^p into x^0 */
+                accum[idx] = 0;                 /* zero x^p */
             }
         }
 
-        /* TODO : This line needs to be here otherwise the result is incorrect,
-         * why? Figure this out mister */
-        reduce_coefficients(accum, NTT_Q);
-
-        // Store the result
-        C[0][idx] = accum[0];
-        C[1][idx] = accum[1];
-        C[2][idx] = accum[2];
+        /* Store the result */
+        C_mat[0][idx] = accum[0];
+        C_mat[1][idx] = accum[1];
+        C_mat[2][idx] = accum[2];
     }
 
-    /* -- Inverse GP0 size - GP1 NTTs "*/
+    /**
+     * @brief Compute the iterative inplace inverse NTT.
+     * 
+     * This computes the inverse NTT tranformation of our size-512 polynomials.
+     */
 
-    ntt_inverse(C[0], NTT_Q);
-    ntt_inverse(C[1], NTT_Q);
-    ntt_inverse(C[2], NTT_Q);
+    for (size_t idx = 0; idx < GP0; idx++)
+    {
+        ntt_inverse(C_mat[idx], NTT_Q);
+    }
 
-    /* -- Undo Good's permutation "*/
+    /**
+     * @brief Compute the inverse Good's permutation.
+     * 
+     * This undoes the forward Good's permutation and constructs an array of
+     * integer coefficients from the deconstructed smaller NTT friendly matrix.
+     */
 
-    int32_t tempA[GPR];
-    goods_inverse(tempA, C);
+    int32_t C_vec[GPR];
 
-    /* -- Reduce mod (x^761 - x - 1) "*/
+    goods_inverse(C_vec, C_mat);
 
-    reduce_terms_761(tempA);
-    reduce_coefficients(tempA, NTRU_Q);
+    /**
+     * @brief Reduce the result of the multiplication mod (x^761 - x - 1).
+     */
 
-    /* -- Store the result "*/
+    reduce_terms_761(C_vec);
+
+    /**
+     * @brief Reduce the integer coefficients mod 4591 and store the result.
+     * 
+     * This loop iterates over the first 761 integer coefficients, reduces them
+     * mod 4591 and stores them. This removes the zero padding.
+     */
 
     int32_t poly_out[NTRU_P];
 
     for (size_t idx = 0; idx < NTRU_P; idx++)
     {
-        poly_out[idx] = tempA[idx];
+        poly_out[idx] = modulo(C_vec[idx], NTRU_Q);
     }
-
-    /* reduce the coefficients properly */
-    for (size_t idx = 0; idx < NTRU_P; idx++)
-    {
-        poly_out[idx] = modulo(poly_out[idx], NTRU_Q);
-    }
-    print_polynomial(poly_out, NTRU_P);
 
     /**
      * @brief Test the result of the computation against the known test values
@@ -825,51 +844,4 @@ not actually computing the result 'point-wise'. Instead we multiply two degree
 
     printf("%s\n", "This is correct!");
     return 0;
-
-    // /**
-    //  * @brief Compute the iterative inplace forward NTT of poly_one, poly_two
-    //  */
-
-    // ntt_forward(poly_one, NTT_Q);
-    // ntt_forward(poly_two, NTT_Q);
-
-    // /**
-    //  * @brief Compute the point-wise multiplication of the integer coefficients
-    //  *
-    //  * The following is used to perform the point-wise multiplication of the
-    //  * integer coefficients of two polynomials and store the (reduced) result.
-    //  * Please note that we did not have to declare a new array, we could have
-    //  * simply reused either poly_one or poly_two. To improve readability
-    //  * however, a new array poly_out is introduced.
-    //  */
-
-    // int32_t poly_out[NTT_P];
-
-    // for (size_t idx = 0; idx < NTT_P; idx++)
-    // {
-    //     poly_out[idx] = multiply_modulo(poly_one[idx], poly_two[idx], NTT_Q);
-    // }
-
-    // /**
-    //  * @brief Compute the iterative inplace inverse NTT of poly_out
-    //  */
-
-    // ntt_inverse(poly_out, NTT_Q);
-
-    // /**
-    //  * @brief Test the result of the computation against the known test values
-    //  */
-
-    // for (size_t idx = 0; idx < NTT_P; idx++)
-    // {
-    //     if (poly_out[idx] != result[idx])
-    //     {
-    //         printf("%s\n", "This is not correct!");
-    //         printf("%s%ld\n", "Error at index: ", idx);
-    //         return -1;
-    //     }
-    // }
-
-    // printf("%s\n", "This is correct!");
-    // return 0;
 }
