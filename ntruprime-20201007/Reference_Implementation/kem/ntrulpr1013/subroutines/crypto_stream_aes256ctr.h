dfd07d42337219adbdfd28af52f5200735b4f79d
jhaasdijk <j.haasdijk@student.ru.nl>
2021-06-16 13:51:02 +0100

    Remove unused files from NTRU LPRime submission package
    This commit removes everything not related to ntrulpr761 from the
    original NTRU LPRIME submission package.

--- ntruprime-20201007/Reference_Implementation/kem/ntrulpr1013/subroutines/crypto_stream_aes256ctr.h
+++
@@ -1,6 +0,0 @@
-#ifndef crypto_stream_aes256ctr_H
-#define crypto_stream_aes256ctr_H
-
-int crypto_stream_aes256ctr(unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
-
-#endif
