0de8b8b7da9649ab5d1a55d40ff1ffe4a3332e2f
jhaasdijk <j.haasdijk@student.ru.nl>
2021-06-24 19:17:04 +0100

    Increase asm_ntt_inverse performance
    This commit reduces the cycle count for the inverse NTT by merging
    layers (minimizing LDR, STR operations) and preloading the required
    roots for 4321.
    
    Some minor additional improvements have been included as well.

--- samples/polymult_c/ntrulpr761/asm_ntt_inverse.s
+++ samples/polymult_c/ntrulpr761/asm_ntt_inverse.s
@@ -9,72 +9,20 @@
 
 /* Provide macro definitions */
 
-.macro _asimd_add_sub q0, q1, q2, v0, v1, v2, addr, offset
-    ldr     \q0, [\addr]            // Load coefficients[idx]
-    ldr     \q1, [\addr, \offset]   // Load coefficients[idx + length]
-    add     \v2, \v0, \v1           // temp + coefficients[idx + length]
-    sub     \v1, \v0, \v1           // temp - coefficients[idx + length]
-    str     \q2, [\addr]            // Store coefficients[idx]
-.endm
-
-.macro _asimd_mul_red q1, v0, v1, v2, v3, addr, offset
-    sqdmulh \v2, \v1, \v0[0]        // Mulhi[a, B]
-    mul     \v3, \v1, \v0[1]        // Mullo[a, B']
-    sqdmulh \v3, \v3, \v0[2]        // Mulhi[M, Mullo[a, B']]
-    sub     \v1, \v2, \v3           // Mulhi[a, B] − Mulhi[M, Mullo[a, B']]
-    str     \q1, [\addr, \offset]   // Store coefficients[idx + length]
-    add     \addr, \addr, #16       // Move to the next chunk
-.endm
-
-.macro __asm_ntt_inverse_layer len, ridx, loops
-    mov     start, x0               // Store *coefficients[0]
-    add     last, x0, #4 * \len     // Store *coefficients[len]
-
-    /* Store layer specific values  */
-
-    add     x3, x1, #4 * \ridx      // ridx, used for indexing B
-    add     x4, x2, #4 * \ridx      // ridx, used for indexing B'
-    mov     x5, #1 * \loops         // loops (NTT_P / len / 2)
-
-    ld1     {v7.s}[0], [x3], #4     // Load precomputed B
-    ld1     {v7.s}[1], [x4], #4     // Load precomputed B'
-
-    1:
-
-    /* Perform the ASIMD arithmetic instructions for an inverse butterfly */
-
-    _asimd_add_sub q0, q1, q2, v0.4s, v1.4s, v2.4s, start, #4 * \len
-    _asimd_mul_red q1, v7.4s v1.4s v2.4s v3.4s, start, #4 * \len
-
-    cmp     last, start             // Check if we have reached the next chunk
-    b.ne    1b
-
-    add     start, last, #4 * \len  // Update pointer to next first coefficient
-    add     last, last, #8 * \len   // Update pointer to next last coefficient
-
-    ld1     {v7.s}[0], [x3], #4     // Load precomputed B
-    ld1     {v7.s}[1], [x4], #4     // Load precomputed B'
-
-    sub     x5, x5, #1              // Decrement loop counter by 1
-    cmp     x5, #0                  // Check wether we are done
-    b.ne    1b
-.endm
-
 .macro __asm_reduce_coefficients
-    dup     v3.4s, M                // Move M into all 4 elements
-    mov     v4.4s[0], M_inv         // Move M_inv into index [0]
-                                    // We don't need to fill all 4 elements
-
     mov     start, x0               // Store *coefficients[0]
-    add	    last, x0, #4 * 512      // Store *coefficients[len]
+
+    dup     v3.4s, M                // Move M into all 4 elements
 
     /* Loop over all coefficients */
 
-    1:
+    .rept 128
+
     ldr	    q0, [start]
-    smull   v1.2d, v0.2s, v4.2s[0]
+
+    smull   v1.2d, v0.2s, v28.2s[3]
     sshr    v2.4s, v0.4s, #31
-    smull2  v5.2d, v0.4s, v4.4s[0]
+    smull2  v5.2d, v0.4s, v28.4s[3]
     uzp2    v1.4s, v1.4s, v5.4s
     sshr    v1.4s, v1.4s, #15
     sub	    v1.4s, v1.4s, v2.4s
@@ -83,21 +31,31 @@
     add	    v2.4s, v0.4s, v3.4s
     bif	    v0.16b, v2.16b, v1.16b
 
-    str	    q0, [start], #4 * 4     // Store the result and move to next chunk
-    cmp	    last, start             // Check whether we are done
-    b.ne    1b
+    str	    q0, [start]
+    add     start, start, #16
+
+    .endr
+
+.endm
+
+.macro butterfly lower, upper, twid1, twid2, t1, t2
+    /* _asimd_sub_add */
+    sub     \t1, \lower, \upper
+    add     \lower, \lower, \upper
+
+    /* _asimd_mul_red */
+    sqdmulh \t2, \t1, \twid1
+    mul     \t1, \t1, \twid2
+    sqdmulh \t1, \t1, v28.4s[2]
+    sub     \upper, \t2, \t1
 .endm
 
 __asm_ntt_inverse:
 
     /* Alias registers for a specific purpose (and readability) */
 
-    start   .req x10    // Store pointer to the first integer coefficient
-    last    .req x11    // Store pointer to the last integer coefficient
-
-    MR_top  .req w12    // Store the precomputed B value for _asimd_mul_red
-    MR_bot  .req w13    // Store the precomputed B' value for _asimd_mul_red
-
+    start   .req x12    // Store pointer to the first integer coefficient
+    temp    .req w13
     M       .req w14    // Store the constant value M = 6984193
     M_inv   .req w15    // Store the constant value M_inv = 20150859
 
@@ -105,13 +63,15 @@ __asm_ntt_inverse:
 
     mov     M, #0x9201              // 6984193 (= M)
     movk    M, #0x6a, lsl #16
-
-    mov     v7.4s[2], M             // M TODO : Redundant statement
+    mov     v28.4s[2], M
 
     mov	    M_inv, #0x7a4b
     movk    M_inv, #0x133, lsl #16  // 20150859 (= M_inv)
+    mov     v28.4s[3], M_inv
 
+    /* Layers 9+8 */
     /* NTT inverse layer 9: length = 1, ridx = 0 */
+    /* NTT inverse layer 8: length = 2, ridx = 256 */
 
     start_l .req x10
     start_s .req x11
@@ -121,16 +81,13 @@ __asm_ntt_inverse:
 
     /* Store layer specific values  */
 
-    add     x6, x1, #4 * 0          // ridx, used for indexing B
-    add     x7, x2, #4 * 0          // ridx, used for indexing B'
-    mov     x3, #1 * 64             // 512 / 8 = 64
+    add     x3, x1, #4 * 0          // LAYER 9: ridx, used for indexing B
+    add     x4, x2, #4 * 0          // LAYER 9: ridx, used for indexing B'
 
-    1:
+    add     x5, x1, #4 * 256        // LAYER 8: ridx, used for indexing B
+    add     x6, x2, #4 * 256        // LAYER 8: ridx, used for indexing B'
 
-    ldr q5, [x6], #16               // Load precomputed B
-    ldr q6, [x7], #16               // Load precomputed B'
-
-    /* Load the coefficients */
+    .rept 64
 
     ld1     {v1.s}[0], [start_l], #4
     ld1     {v0.s}[0], [start_l], #4
@@ -140,117 +97,138 @@ __asm_ntt_inverse:
     ld1     {v0.s}[2], [start_l], #4
     ld1     {v1.s}[3], [start_l], #4
     ld1     {v0.s}[3], [start_l], #4
+
+    // LAYER 9
+
+    ldr     q24, [x3], #16
+    ldr     q25, [x4], #16
+
+    sub     v30.4s, v1.4s, v0.4s
+    add     v1.4s, v1.4s, v0.4s
+
+    sqdmulh v31.4s, v30.4s, v24.4s      // Mulhi[a, B]
+    mul     v30.4s, v30.4s, v25.4s      // Mullo[a, B']
+    sqdmulh v30.4s, v30.4s, v28.4s[2]   // Mulhi[M, Mullo[a, B']]
+    sub     v0.4s, v31.4s, v30.4s       // Mulhi[a, B] − Mulhi[M, Mullo[a, B']]
 
     // q0 contains coefficients [1, 3, 5, 7]
     // q1 contains coefficients [0, 2, 4, 6]
 
-    /* Execute _asimd_add_sub */
+    mov     v2.s[0], v1.s[1]
+    mov     v2.s[1], v0.s[1]
+    mov     v2.s[2], v1.s[3]
+    mov     v2.s[3], v0.s[3]
 
-    add     v2.4s, v1.4s, v0.4s
-    sub     v0.4s, v1.4s, v0.4s
+    mov     v3.s[0], v1.s[0]
+    mov     v3.s[1], v0.s[0]
+    mov     v3.s[2], v1.s[2]
+    mov     v3.s[3], v0.s[2]
 
-    /* Execute _asimd_mul_red */
+    // q2 contains coefficients [2, 3, 6, 7]
+    // q3 contains coefficients [0, 1, 4, 5]
 
-    sqdmulh v1.4s, v0.4s, v5.4s     // Mulhi[a, B]
-    mul     v3.4s, v0.4s, v6.4s     // Mullo[a, B']
-    sqdmulh v3.4s, v3.4s, v7.4s[2]  // Mulhi[M, Mullo[a, B']]
-    sub     v1.4s, v1.4s, v3.4s     // Mulhi[a, B] − Mulhi[M, Mullo[a, B']]
+    // LAYER 8
 
-    // q2 contains coefficients [0, 2, 4, 6]
-    // q1 contains coefficients [1, 3, 5, 7]
+    ld1     {v26.s}[0], [x5]
+    ld1     {v26.s}[1], [x5], #4
+    ld1     {v26.s}[2], [x5]
+    ld1     {v26.s}[3], [x5], #4
 
-    /* Store the result */
+    ld1     {v27.s}[0], [x6]
+    ld1     {v27.s}[1], [x6], #4
+    ld1     {v27.s}[2], [x6]
+    ld1     {v27.s}[3], [x6], #4
 
+    sub     v30.4s, v3.4s, v2.4s
+    add     v3.4s, v3.4s, v2.4s
+
+    sqdmulh v31.4s, v30.4s, v26.4s      // Mulhi[a, B]
+    mul     v30.4s, v30.4s, v27.4s      // Mullo[a, B']
+    sqdmulh v30.4s, v30.4s, v28.4s[2]   // Mulhi[M, Mullo[a, B']]
+    sub     v2.4s, v31.4s, v30.4s       // Mulhi[a, B] − Mulhi[M, Mullo[a, B']]
+
+    st1     {v3.s}[0], [start_s], #4
+    st1     {v3.s}[1], [start_s], #4
     st1     {v2.s}[0], [start_s], #4
-    st1     {v1.s}[0], [start_s], #4
     st1     {v2.s}[1], [start_s], #4
-    st1     {v1.s}[1], [start_s], #4
+    st1     {v3.s}[2], [start_s], #4
+    st1     {v3.s}[3], [start_s], #4
     st1     {v2.s}[2], [start_s], #4
-    st1     {v1.s}[2], [start_s], #4
     st1     {v2.s}[3], [start_s], #4
-    st1     {v1.s}[3], [start_s], #4
 
-    sub     x3, x3, #1              // Decrement loop counter by 1
-    cmp     x3, #0                  // Check wether we are done
-    b.ne    1b
+    .endr
 
-    /* NTT inverse layer 8: length = 2, ridx = 256 */
+    /* Layers 7+6+5 */
+    /* NTT inverse layer 7: length = 4, ridx = 384, loops = 64 */
+    /* NTT inverse layer 6: length = 8, ridx = 448, loops = 32 */
+    /* NTT inverse layer 5: length = 16, ridx = 480, loops = 16 */
 
-    mov     start_l, x0
-    mov     start_s, x0
+    mov     start, x0               // Store *coefficients[0]
 
     /* Store layer specific values  */
 
-    add     x6, x1, #4 * 256        // ridx, used for indexing B
-    add     x7, x2, #4 * 256        // ridx, used for indexing B'
-    mov     x3, #1 * 64             // 512 / 8 = 64
+    add     x3, x1, #4 * 384        // LAYER 7: ridx, used for indexing B
+    add     x4, x2, #4 * 384        // LAYER 7: ridx, used for indexing B'
 
-    1:
+    add     x5, x1, #4 * 448        // LAYER 6: ridx, used for indexing B
+    add     x6, x2, #4 * 448        // LAYER 6: ridx, used for indexing B'
 
-    /* Load the precomputed roots */
+    add     x7, x1, #4 * 480        // LAYER 5: ridx, used for indexing B
+    add     x9, x2, #4 * 480        // LAYER 5: ridx, used for indexing B'
 
-    ld1     {v5.s}[0], [x6]
-    ld1     {v5.s}[1], [x6], #4
-    ld1     {v5.s}[2], [x6]
-    ld1     {v5.s}[3], [x6], #4
+    .rept 16
 
-    ld1     {v6.s}[0], [x7]
-    ld1     {v6.s}[1], [x7], #4
-    ld1     {v6.s}[2], [x7]
-    ld1     {v6.s}[3], [x7], #4
+    ldr     q0, [start, #4 * 0]
+    ldr     q1, [start, #4 * 4]
+    ldr     q2, [start, #4 * 8]
+    ldr     q3, [start, #4 * 12]
+    ldr     q4, [start, #4 * 16]
+    ldr     q5, [start, #4 * 20]
+    ldr     q6, [start, #4 * 24]
+    ldr     q7, [start, #4 * 28]
 
-    /* Load the coefficients */
+    // LAYER 7
 
-    ld1     {v1.s}[0], [start_l], #4
-    ld1     {v1.s}[1], [start_l], #4
-    ld1     {v0.s}[0], [start_l], #4
-    ld1     {v0.s}[1], [start_l], #4
-    ld1     {v1.s}[2], [start_l], #4
-    ld1     {v1.s}[3], [start_l], #4
-    ld1     {v0.s}[2], [start_l], #4
-    ld1     {v0.s}[3], [start_l], #4
+    ldr     q24, [x3], #16
+    ldr     q25, [x4], #16
+    butterfly v0.4s, v1.4s, v24.4s[0], v25.4s[0], v30.4s, v31.4s
+    butterfly v2.4s, v3.4s, v24.4s[1], v25.4s[1], v30.4s, v31.4s
+    butterfly v4.4s, v5.4s, v24.4s[2], v25.4s[2], v30.4s, v31.4s
+    butterfly v6.4s, v7.4s, v24.4s[3], v25.4s[3], v30.4s, v31.4s
 
-    // q0 contains coefficients [2, 3, 6, 7]
-    // q1 contains coefficients [0, 1, 4, 5]
+    // LAYER 6
 
-    /* Execute _asimd_add_sub */
+    ld1     {v28.s}[0], [x5], #4
+    ld1     {v28.s}[1], [x6], #4
+    butterfly v0.4s, v2.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
+    butterfly v1.4s, v3.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
 
-    add     v2.4s, v1.4s, v0.4s
-    sub     v0.4s, v1.4s, v0.4s
+    ld1     {v28.s}[0], [x5], #4
+    ld1     {v28.s}[1], [x6], #4
+    butterfly v4.4s, v6.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
+    butterfly v5.4s, v7.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
 
-    /* Execute _asimd_mul_red */
+    // LAYER 5
 
-    sqdmulh v1.4s, v0.4s, v5.4s     // Mulhi[a, B]
-    mul     v3.4s, v0.4s, v6.4s     // Mullo[a, B']
-    sqdmulh v3.4s, v3.4s, v7.4s[2]  // Mulhi[M, Mullo[a, B']]
-    sub     v1.4s, v1.4s, v3.4s     // Mulhi[a, B] − Mulhi[M, Mullo[a, B']]
+    ld1     {v28.s}[0], [x7], #4
+    ld1     {v28.s}[1], [x9], #4
+    butterfly v0.4s, v4.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
+    butterfly v1.4s, v5.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
+    butterfly v2.4s, v6.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
+    butterfly v3.4s, v7.4s, v28.4s[0], v28.4s[1], v30.4s, v31.4s
 
-    // q2 contains coefficients [0, 1, 4, 5]
-    // q1 contains coefficients [2, 3, 6, 7]
+    str     q0, [start, #4 * 0]
+    str     q1, [start, #4 * 4]
+    str     q2, [start, #4 * 8]
+    str     q3, [start, #4 * 12]
+    str     q4, [start, #4 * 16]
+    str     q5, [start, #4 * 20]
+    str     q6, [start, #4 * 24]
+    str     q7, [start, #4 * 28]
 
-    /* Store the result */
+    add     start, start, #4 * 32   // Update pointer to next first coefficient
 
-    st1     {v2.s}[0], [start_s], #4
-    st1     {v2.s}[1], [start_s], #4
-    st1     {v1.s}[0], [start_s], #4
-    st1     {v1.s}[1], [start_s], #4
-    st1     {v2.s}[2], [start_s], #4
-    st1     {v2.s}[3], [start_s], #4
-    st1     {v1.s}[2], [start_s], #4
-    st1     {v1.s}[3], [start_s], #4
-
-    sub     x3, x3, #1              // Decrement loop counter by 1
-    cmp     x3, #0                  // Check wether we are done
-    b.ne    1b
-
-    /* NTT inverse layer 7: length = 4, ridx = 384, loops = 64 */
-    __asm_ntt_inverse_layer 4, 384, 64
-
-    /* NTT inverse layer 6: length = 8, ridx = 448, loops = 32 */
-    __asm_ntt_inverse_layer 8, 448, 32
-
-    /* NTT inverse layer 5: length = 16, ridx = 480, loops = 16 */
-    __asm_ntt_inverse_layer 16, 480, 16
+    .endr
 
     /**
      * @brief Ensure that the coefficients stay within their allocated 32 bits
@@ -266,58 +244,146 @@ __asm_ntt_inverse:
 
     __asm_reduce_coefficients
 
+    /* Layers 4+3+2+1 */
     /* NTT inverse layer 4: length = 32, ridx = 496, loops = 8 */
-    __asm_ntt_inverse_layer 32, 496, 8
-
     /* NTT inverse layer 3: length = 64, ridx = 504, loops = 4 */
-    __asm_ntt_inverse_layer 64, 504, 4
-
     /* NTT inverse layer 2: length = 128, ridx = 508, loops = 2 */
-    __asm_ntt_inverse_layer 128, 508, 2
-
     /* NTT inverse layer 1: length = 256, ridx = 510, loops = 1 */
 
     mov     start, x0               // Store *coefficients[0]
-    add     last, x0, #4 * 256      // Store *coefficients[256]
 
-    add     x3, x1, #4 * 510
-    add     x4, x2, #4 * 510
-    ld1     {v7.s}[0], [x3], #4     // Load precomputed B
-    ld1     {v7.s}[1], [x4], #4     // Load precomputed B'
+    /* Store layer specific values  */
 
-    1:
+    add     x3, x1, #4 * 496        // Store *B[496]
+    add     x4, x2, #4 * 496        // Store *B'[496]
 
-    /* Perform the ASIMD arithmetic instructions for a inverse butterfly */
+    /* Preload the required root values, we have enough room */
+    /* This works because there are actually only 16 different values */
+    /* [496] == [504] == [508] == [510] */
+    /* [497] == [505] == [509] */
+    /* [498] == [506] */
+    /* [499] == [507] */
 
-    _asimd_add_sub q0, q1, q2, v0.4s, v1.4s, v2.4s, start, #4 * 256
-    _asimd_mul_red q1, v7.4s v1.4s v2.4s v3.4s, start, #4 * 256
+    ldr     q24, [x3], #4 * 4       // B[496, 497, 498, 499]
+    ldr     q25, [x3], #4 * 4       // B[500, 501, 502, 503]
+    ldr     q26, [x4], #4 * 4       // B'[496, 497, 498, 499]
+    ldr     q27, [x4]               // B'[500, 501, 502, 503]
 
-    cmp     last, start             // Compare offset with *coefficients[256]
-    b.ne    1b
+    .rept 8
+
+    ldr     q0, [start, #4 * 0]
+    ldr     q1, [start, #4 * 32]
+    ldr     q2, [start, #4 * 64]
+    ldr     q3, [start, #4 * 96]
+
+    ldr     q4, [start, #4 * 128]
+    ldr     q5, [start, #4 * 160]
+    ldr     q6, [start, #4 * 192]
+    ldr     q7, [start, #4 * 224]
+
+    ldr     q16, [start, #4 * 256]
+    ldr     q17, [start, #4 * 288]
+    ldr     q18, [start, #4 * 320]
+    ldr     q19, [start, #4 * 352]
+
+    ldr     q20, [start, #4 * 384]
+    ldr     q21, [start, #4 * 416]
+    ldr     q22, [start, #4 * 448]
+    ldr     q23, [start, #4 * 480]
+
+    // LAYER 4
+
+    butterfly v0.4s, v1.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v2.4s, v3.4s, v24.4s[1], v26.4s[1], v30.4s, v31.4s
+    butterfly v4.4s, v5.4s, v24.4s[2], v26.4s[2], v30.4s, v31.4s
+    butterfly v6.4s, v7.4s, v24.4s[3], v26.4s[3], v30.4s, v31.4s
+    butterfly v16.4s, v17.4s, v25.4s[0], v27.4s[0], v30.4s, v31.4s
+    butterfly v18.4s, v19.4s, v25.4s[1], v27.4s[1], v30.4s, v31.4s
+    butterfly v20.4s, v21.4s, v25.4s[2], v27.4s[2], v30.4s, v31.4s
+    butterfly v22.4s, v23.4s, v25.4s[3], v27.4s[3], v30.4s, v31.4s
+
+    // LAYER 3
+
+    butterfly v0.4s, v2.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v1.4s, v3.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v4.4s, v6.4s, v24.4s[1], v26.4s[1], v30.4s, v31.4s
+    butterfly v5.4s, v7.4s, v24.4s[1], v26.4s[1], v30.4s, v31.4s
+    butterfly v16.4s, v18.4s, v24.4s[2], v26.4s[2], v30.4s, v31.4s
+    butterfly v17.4s, v19.4s, v24.4s[2], v26.4s[2], v30.4s, v31.4s
+    butterfly v20.4s, v22.4s, v24.4s[3], v26.4s[3], v30.4s, v31.4s
+    butterfly v21.4s, v23.4s, v24.4s[3], v26.4s[3], v30.4s, v31.4s
+
+    // LAYER 2
+
+    butterfly v0.4s, v4.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v1.4s, v5.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v2.4s, v6.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v3.4s, v7.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v16.4s, v20.4s, v24.4s[1], v26.4s[1], v30.4s, v31.4s
+    butterfly v17.4s, v21.4s, v24.4s[1], v26.4s[1], v30.4s, v31.4s
+    butterfly v18.4s, v22.4s, v24.4s[1], v26.4s[1], v30.4s, v31.4s
+    butterfly v19.4s, v23.4s, v24.4s[1], v26.4s[1], v30.4s, v31.4s
+
+    // LAYER 1
+
+    butterfly v0.4s, v16.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v1.4s, v17.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v2.4s, v18.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v3.4s, v19.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v4.4s, v20.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v5.4s, v21.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v6.4s, v22.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+    butterfly v7.4s, v23.4s, v24.4s[0], v26.4s[0], v30.4s, v31.4s
+
+    str     q0, [start, #4 * 0]
+    str     q1, [start, #4 * 32]
+    str     q2, [start, #4 * 64]
+    str     q3, [start, #4 * 96]
+
+    str     q4, [start, #4 * 128]
+    str     q5, [start, #4 * 160]
+    str     q6, [start, #4 * 192]
+    str     q7, [start, #4 * 224]
+
+    str     q16, [start, #4 * 256]
+    str     q17, [start, #4 * 288]
+    str     q18, [start, #4 * 320]
+    str     q19, [start, #4 * 352]
+
+    str     q20, [start, #4 * 384]
+    str     q21, [start, #4 * 416]
+    str     q22, [start, #4 * 448]
+    str     q23, [start, #4 * 480]
+
+    add     start, start, #16       // Move to the next chunk
+
+    .endr
 
     /* Multiply the result with the accumulated factor to complete the NTT */
 
     mov     start, x0
-    add     last, x0, #4 * 512
 
     // 512^-1 mod 6984193     = 6970552
     // B  = 6970552 · R mod M = 4194304
     // B' = B · M' mod R      = 4194304
+    // Move wide with zero, 4194304 = 0x400000
 
-    movz    MR_top, #0x40, lsl #16  // Move wide with zero, 4194304 = 0x400000
-    mov     v3.4s[0], MR_top
+    movz    temp, #0x40, lsl #16
+    mov     v24.4s[0], temp
 
-    2:
+    .rept 128
 
-    ldr     q0, [start]             // Load coefficients[idx]
-    sqdmulh v1.4s, v0.4s, v3.4s[0]  // Execute multiply_reduce
-    mul     v2.4s, v0.4s, v3.4s[0]
-    sqdmulh v2.4s, v2.4s, v7.4s[2]
-    sub     v0.4s, v1.4s, v2.4s
-    str     q0, [start], #16        // Store coefficients[idx]
+    ldr     q0, [start]
 
-    cmp     last, start
-    b.ne    2b
+    sqdmulh v30.4s, v0.4s, v24.4s[0]
+    mul     v31.4s, v0.4s, v24.4s[0]
+    sqdmulh v31.4s, v31.4s, v28.4s[2]
+    sub     v0.4s, v30.4s, v31.4s
+
+    str     q0, [start]
+    add     start, start, #16
+
+    .endr
 
     __asm_reduce_coefficients
 
